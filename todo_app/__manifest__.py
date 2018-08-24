69ca9c20cfc168f015c4d768854abea423e24a42
MacauleyTech <37306854+MacauleyTech@users.noreply.github.com>
2018-08-24 16:47:44 -0500

    to-do test

--- todo_app/__manifest__.py
+++ todo_app/__manifest__.py
@@ -1,34 +1,9 @@
 # -*- coding: utf-8 -*-
-{
-    'name': "template_module",
-
-    'summary': """
-        Template to copy when starting a new module""",
-
-    'description': """
-        Long description of module's purpose
-    """,
-
-    'author': "Macauley Technologies",
-    'website': "http://www.macauleytechnologies.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base'],
-
-    # always loaded
-    'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+{ 
+    'name': 'To-Do Application', 
+    'description': 'Manage personal to-do tasks.', 
+    'author': 'Cliff', 
+    'depends': ['base'], 
+    'application': True,
+    'version':11.0.1.0,
 }
