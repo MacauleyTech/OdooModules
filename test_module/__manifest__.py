e520d3fdc5a0b1884768332bd8a8e2a73061de3c
Cliff Clarke <cclarke@macauleyllc.com>
2018-08-25 16:11:08 -0500

    test app creation

---
+++ test_module/__manifest__.py
@@ -0,0 +1,8 @@
+{ 
+    'name': 'To-Do Application', 
+    'description': 'Manage personal to-do tasks.', 
+    'author': 'Daniel Reis', 
+    'depends': ['base'], 
+    'application': True,
+    'version': '11.0.0.1',
+}
