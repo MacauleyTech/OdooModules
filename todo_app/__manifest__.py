950a5370983df3127c5d54772114902243fc60a4
Cliff Clarke <cclarke@macauleyllc.com>
2018-08-25 16:01:08 -0500

    remove ToDo

--- todo_app/__manifest__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-{ 
-    'name': 'To-Do Application', 
-    'description': 'Manage personal to-do tasks.', 
-    'author': 'Cliff', 
-    'depends': ['base'], 
-    'application': True,
-    'version':11.0.1.0
-}
